6444346dd5d830f2cca1c30f0c4b6bed8efd58a2
Andrey <akart.dev@gmail.com>
2022-11-17 18:17:56 +0600

    feat: create add form

---
+++ angular-test/src/app/components/create-product/create-product.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.scss']
+})
+
+export class CreateProductComponent implements OnInit {
+
+  form = new FormGroup({
+    title: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  })
+
+  get title() {
+    return this.form.controls.title as FormControl
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+    console.log(this.form.value)
+  }
+
+}
